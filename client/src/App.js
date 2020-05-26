2b63bc009827a6d254116260c6ce6a0b68168897
Terry Mac-Tay <terry.mactay@gmail.com>
2020-05-26 14:31:22 +0300

    added newfile

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,20 @@ import './App.css';
 import Jobs from './Jobs';
 
 const JOB_API_URL = 'http://localhost:3001/jobs'
-
+/* 
 const mockJobs = [
   {title: 'SWE 1',  company: 'Google'},
   {title: 'SWE 1',  company: 'Glassdoor'},
   {title: 'SWE 1',  company: 'Facebook'},
   {title: 'SWE 1',  company: 'Linkedin'}
-]
+] */
 
 async function fetchJobs(updateCallback){
   const res = await fetch(JOB_API_URL);
   const json = await res.json();
+  console.log({json});
 
   updateCallback(json);
-
-  console.log({json});
 }
 
 
